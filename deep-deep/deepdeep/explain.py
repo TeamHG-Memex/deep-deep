2feee6782f79b53a147c6390aab0788649cc2f15
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-11-07 15:53:04 +0300

    Update to recent eli5 changes

--- deep-deep/deepdeep/explain.py
+++ deep-deep/deepdeep/explain.py
@@ -1,11 +1,12 @@
 from typing import Dict, List, Tuple, Union
 
 from scrapy.http.response.text import TextResponse
-from eli5.sklearn import explain_prediction
+from eli5.base import WeightedSpans, FeatureWeights
+from eli5.sklearn import explain_prediction_sklearn
 from eli5.sklearn.text import get_weighted_spans
 from eli5.sklearn.unhashing import InvertableHashingVectorizer
 from eli5.sklearn.utils import FeatureNames
-from eli5.formatters import EscapedFeatureName
+from eli5.formatters import FormattedFeatureName
 import numpy as np
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.pipeline import FeatureUnion
@@ -57,52 +58,59 @@ def links_explanations(clf, vec: FeatureUnion, links: List[Dict]) -> List[Dict]:
     all_expl = []
     feature_names = get_feature_names_scales(vec, links, with_scales=False)
     for link in links:
-        expl = explain_prediction(
+        expl = explain_prediction_sklearn(
             clf, link, vec, feature_names=feature_names, top=1000)
-        target_expl = expl['targets'][0]
-        add_weighted_spans(link, target_expl, vec)
-        all_expl.append((target_expl['score'], link, expl))
+        target_expl = expl.targets[0]
+        target_expl.weighted_spans = combined_weighted_spans(
+            link, target_expl, vec)
+        all_expl.append((target_expl.score, link, expl))
     return all_expl
 
 
-def add_weighted_spans(link, target_expl, vectorizer):
-    ws_combined = {
-        'document': '', 'weighted_spans': [], 'other': {'pos': [], 'neg': []}}
+def combined_weighted_spans(link, target_expl, vectorizer):
+    ws_combined = WeightedSpans(
+        analyzer='',
+        document='',
+        weighted_spans=[],
+        other=FeatureWeights(pos=[], neg=[]))
     for vec_idx in [0, 2]:
         vec = vectorizer.transformer_list[vec_idx][1]
         vec_name = vec.preprocessor.__name__
-        feature_weights = {key: [
-                (name if isinstance(name, str) else
-                 [n for n in name if n['vec'] == vec],
-                 coef)
-                for name, coef in target_expl['feature_weights'][key]]
-                for key in ['pos', 'neg']
-                }
+        get_weights = lambda weights: [
+            (name if isinstance(name, str) else
+             [n for n in name if n['vec'] == vec],
+             coef) for name, coef in weights]
+        feature_weights = FeatureWeights(
+            pos=get_weights(target_expl.feature_weights.pos),
+            neg=get_weights(target_expl.feature_weights.neg))
         ws = get_weighted_spans(
             link, vec=vec, feature_weights=feature_weights)
-        ws_combined['analyzer'] = ws['analyzer']
-        if ws_combined['document']:
-            ws_combined['document'] += ' | '
-        s0 = len(ws_combined['document'])
-        ws_combined['document'] += ws['document']
+        ws_combined.analyzer = ws.analyzer
+        if ws_combined.document:
+            ws_combined.document += ' | '
+        s0 = len(ws_combined.document)
+        ws_combined.document += ws.document
         shifted_spans = [
             (feature, [(s0 + s, s0 + e) for s, e in spans], weight)
-            for feature, spans, weight in ws['weighted_spans']]
-        ws_combined['weighted_spans'].extend(shifted_spans)
-        for key, other in ws_combined['other'].items():
+            for feature, spans, weight in ws.weighted_spans]
+        ws_combined.weighted_spans.extend(shifted_spans)
+        for combined_other_weights, other_weights in [
+                (ws_combined.other.pos, ws.other.pos),
+                (ws_combined.other.neg, ws.other.neg)]:
             # Ignore pos_remaining and neg_remaining, top should be large enough
-            other_features = {f for f, _ in other}
-            for f, w in ws['other'][key]:
+            other_features = {f for f, _ in combined_other_weights}
+            for f, w in other_weights:
                 if str(f) == 'Highlighted in text (sum)':
-                    f = EscapedFeatureName('{}, {}'.format(f, vec_name))
-                    other.append((f, w))
+                    f = FormattedFeatureName('{}, {}'.format(f, vec_name))
+                    combined_other_weights.append((f, w))
                 elif f not in other_features:
-                    other.append((f, w))
-    found_features = {f for f, _, _ in ws_combined['weighted_spans']}
-    for other in ws_combined['other'].values():
-        other[:] = sorted(((f, w) for f, w in other if f not in found_features),
-                          key=lambda x: abs(x[1]), reverse=True)
-    target_expl['weighted_spans'] = ws_combined
+                    combined_other_weights.append((f, w))
+    found_features = {f for f, _, _ in ws_combined.weighted_spans}
+    for other_weights in [ws_combined.other.pos, ws_combined.other.neg]:
+        other_weights[:] = sorted(
+            ((f, w) for f, w in other_weights if f not in found_features),
+            key=lambda x: abs(x[1]), reverse=True)
+    return ws_combined
 
 
 def extract_links(le: DictLinkExtractor, response: TextResponse) -> List[Dict]:
