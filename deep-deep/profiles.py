b449cb4ff0cbf8db61311551e7b5c89956582ed0
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-01-31 13:40:41 +0300

    cli for crawl script generation

--- deep-deep/profiles.py
+++ deep-deep/profiles.py
@@ -1,3 +1,4 @@
+import argparse
 from itertools import islice
 from pathlib import Path
 import re
@@ -52,35 +53,6 @@ def extract_username(response: TextResponse):
         yield username, None
 
 
-def make_script(experiment_root: Path, top: int, use_page_urls: bool=True):
-    print('set -v')
-    for pattern, username, _, _ in islice(_rules_reader(RULES_PATH), top):
-        parsed = urlsplit(pattern)
-        profile_url = pattern % username
-        root_url = '{}://{}'.format(parsed.scheme, parsed.netloc)
-        domain = get_domain(root_url)
-        root = experiment_root / domain
-        if root.exists():
-            assert not any(root.iterdir())
-        else:
-            root.mkdir(parents=True)
-        seeds_path = experiment_root.joinpath('{}-seeds.txt'.format(domain))
-        seeds_path.write_text('\n'.join([root_url, profile_url, '']))
-        print(
-            'scrapy crawl extraction -a extractor=profiles:extract_username '
-            "-a seeds_url='{seeds_url}' "
-            '-a checkpoint_path={root} '
-            '-a use_page_urls={use_page_urls} '
-            '-s LOG_LEVEL=INFO -s LOG_FILE={root}/spider.log '
-            '-s CLOSESPIDER_ITEMCOUNT=0 '  # no limit
-            '-o {root}/{domain}.jl &'
-            .format(seeds_url=seeds_path.absolute(),
-                    domain=domain,
-                    root=root,
-                    use_page_urls=int(use_page_urls),
-                    ))
-
-
 def merge_profiles():
     # FIXME - copied from ipython
     import json, csv, gzip
@@ -111,5 +83,48 @@ def merge_profiles():
             writer.writerow([url, name])
 
 
+def make_script(experiment_root: Path, limit: int, offset: int,
+                use_page_urls: bool=True):
+    print('set -v')
+    for pattern, username, _, _ in islice(
+            _rules_reader(RULES_PATH), offset, offset + limit):
+        parsed = urlsplit(pattern)
+        profile_url = pattern % username
+        root_url = '{}://{}'.format(parsed.scheme, parsed.netloc)
+        domain = get_domain(root_url)
+        root = experiment_root / domain
+        if root.exists():
+            assert not any(root.iterdir())
+        else:
+            root.mkdir(parents=True)
+        seeds_path = experiment_root.joinpath('{}-seeds.txt'.format(domain))
+        seeds_path.write_text('\n'.join([root_url, profile_url, '']))
+        print(
+            'scrapy crawl extraction -a extractor=profiles:extract_username '
+            "-a seeds_url='{seeds_url}' "
+            '-a checkpoint_path={root} '
+            '-a use_page_urls={use_page_urls} '
+            '-s LOG_LEVEL=INFO -s LOG_FILE={root}/spider.log '
+            '-s CLOSESPIDER_ITEMCOUNT=0 '  # no limit
+            '-o {root}/{domain}.jl &'
+            .format(seeds_url=seeds_path.absolute(),
+                    domain=domain,
+                    root=root,
+                    use_page_urls=int(use_page_urls),
+                    ))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg('checkpoint_root', type=Path)
+    arg('--limit', type=int, default=16)
+    arg('--offset', type=int, default=0)
+    arg('--use-page-urls', type=int, default=1)
+    args = parser.parse_args()
+    make_script(args.checkpoint_root, limit=args.limit, offset=args.offset,
+                use_page_urls=args.use_page_urls)
+
+
 if __name__ == '__main__':
-    make_script(Path('profiles/user-root-seeds'), top=16)
+    main()
